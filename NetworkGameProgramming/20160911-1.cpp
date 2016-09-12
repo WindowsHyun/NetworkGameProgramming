da9eb3c9e73a3670e9052fdfaa3fa9c1da4efdbb
WindowsHyun <kwon616@gmail.com>
2016-09-12 11:45:39 +0900

    2016년 09월 12일 도메인 을 ip주소로 변환

--- NetworkGameProgramming/20160911-1.cpp
+++ NetworkGameProgramming/20160911-1.cpp
@@ -1,4 +1,3 @@
-#pragma comment(lib, "ws2_32")
 #include <WinSock2.h>
 #include <stdio.h>
 /*
@@ -8,7 +7,7 @@
 BOOL IsLittleEndian() {
 	// 리턴 값 : 리틀 엔디안이면 True, 그렇지 않으면 FALSE를 리턴한다.
 	// 리틀 엔디안은 뒤에서 부터 읽고 쓴다
-	int num = 0x41470566; 
+	int num = 0x41470566;
 	if ( *((char*)&num) == 0x66 ) { 
 		return true;
 	}
@@ -38,12 +37,11 @@ int main( ) {
 		printf( "BigEndian 입니다..!\n" );
 	}
 
-
 	return 0;
 }
 
 /*
 0x41470566 16진수를 int 4바이트에 넣어서
 char로 1바이트로 불러올시 66 05 47 41 이렇게 끊어져서 나온다.
-num의 주소를 char 포인터 형으로 변환하면 *연산자로 역참조를 하였을 때 num의 전체 바이트 중에 1바이트를 참조하고 있을것입니다, 
+num의 주소를 char 포인터 형으로 변환하면 *연산자로 역참조를 하였을 때 num의 전체 바이트 중에 1바이트를 참조하고 있다.
 */
